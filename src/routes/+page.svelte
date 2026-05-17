6557a6750bb8bedb532dcc8709741cdb34cba830
Linhua <i@linhua.net>
2026-05-17 17:23:33 +0800

    svelte-ux menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,67 @@
+<script lang="ts">
+    import {
+        mdiChevronDown,
+        mdiContentCopy,
+        mdiContentCut,
+        mdiContentPaste,
+    } from "@mdi/js";
+    import {
+        Button,
+        Icon,
+        Menu,
+        MenuButton,
+        MenuField,
+        MenuItem,
+        Stack,
+        Toggle,
+    } from "svelte-ux";
+
+    const options = [
+        { label: "Cut", value: "cut" },
+        { label: "Copy", value: "copy" },
+        { label: "Paste", value: "paste" },
+    ];
+
+    const optionsWithIcons = [
+        { label: "Cut", value: "cut", icon: mdiContentCut },
+        { label: "Copy", value: "copy", icon: mdiContentCopy },
+        { label: "Paste", value: "paste", icon: mdiContentPaste },
+    ];
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Stack horizontal gap="md">
+    <MenuButton options={optionsWithIcons}>
+        <svelte:fragment slot="selection" let:value>
+            {#if value}
+                <Icon data={value?.icon ?? mdiChevronDown} /> {value.label}
+            {:else}
+                No selection
+            {/if}
+        </svelte:fragment>
+    </MenuButton>
+
+    <MenuButton options={optionsWithIcons} menuIcon={null}>
+        <svelte:fragment slot="selection" let:value>
+            <Icon data={value?.icon ?? mdiChevronDown} />
+        </svelte:fragment>
+    </MenuButton>
+
+    <MenuButton {options} variant="fill" color="primary" />
+
+    <Toggle let:on={open} let:toggle let:toggleOff>
+        <Button on:click={toggle}>
+            Click me
+            <Menu {open} on:close={toggleOff}>
+                <MenuItem>Refresh</MenuItem>
+                <MenuItem>Settings</MenuItem>
+                <MenuItem>Help</MenuItem>
+                <MenuItem>Sign In</MenuItem>
+                <MenuItem disabled>Disabled</MenuItem>
+            </Menu>
+        </Button>
+    </Toggle>
+
+    <MenuField label="View" {options} />
+</Stack>
